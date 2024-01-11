b078586c2f248e57b746f20e4ea2ee8125f033e2
amaurineo <amaurineto8@gmail.com>
2024-01-11 18:05:40 -0300

    Primeiro Commit pós tutorial, muito ainda a ser feito

---
+++ README.md
@@ -0,0 +1,14 @@
+# Functional responsive personal portfolio website
+Fully functional responsive personal portfolio website design using HTML, CSS, and JavaScript
+
+## [▶️ Watch Complete Tutorial on YouTube](https://youtu.be/M8jCxWH2zPI)
+### Learn to Code
+
+Learn how to create a fully functional responsive personal portfolio website using HTML, CSS, and JavaScript from scretch by watching this step by stap tutorial video. Don't forget to Subscribe to my YouTube channel for getting more web development tutorial videos.
+
+🎁 Get Design Source Code from [Here](https://www.buymeacoffee.com/the.codermj/e/197293)
+
+Thanks,
+[codermj](https://www.youtube.com/@the.codermj/)
+
+![responsive personal portfolio website](https://github.com/mjshofy/responsive-portfolio-website-creatify/assets/76812554/9f96745a-db89-4b3a-8691-412d88e2db4a)
---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// selecting the html elements
+const hambIcon = document.querySelector(".hamb");
+const navMobile = document.querySelector(".nav")
+
+// activating the hamburger and mobile nav
+hambIcon.addEventListener("click",function(){
+    hambIcon.classList.toggle("active");
+    navMobile.classList.toggle("slide");
+});
+
+// hide mobile navigation after clicking a link or clicking outside
+document.onclick = function (n) {
+    if(n.target.id !== "hamb" && n.target.it !== "nav__menu"){
+        hambIcon.classList.remove("active")
+        navMobile.classList.remove("slide")
+    }
+}
+
+
+// adding sticky animation to the navigation
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", scrollY > 0)
+});
+
+// mixitup for filtering the portfolio works
+const containEl = document.querySelector(".portfolio__gallery");
+
+var mixer = mixitup(containEl, {
+    animation:{
+        enable: false,
+    },
+});
+
+mixer.filter("*");
